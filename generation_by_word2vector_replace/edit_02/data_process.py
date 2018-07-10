e9d5a0c9274e08e0003592a228e19d3c1dca2b1d
tantao258 <823232162@qq.com>
2018-07-10 15:38:06 +0800

    updata

--- generation_by_word2vector_replace/edit_02/data_process.py
+++ generation_by_word2vector_replace/edit_02/data_process.py
@@ -1,67 +1,56 @@
 import os
 import re
 import jieba
-import gensim
+import argparse
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
-import multiprocessing
 
-
-process_data=False
-train_word2vector=True
-
-#========================================================================文本批处理==========================================================
+process_data = False
+train_word2vector = False
+# ========================================================================文本批处理==========================================================
 if process_data:
-    rootDir="corpus"
-    filepath="/data/books/"
-    for dir in os.listdir(filepath):
-        if not os.path.exists(os.path.join(rootDir, dir)):
-            os.makedirs(os.path.join(rootDir, dir))
-        if dir=='女频':
-            continue
-        for item in os.listdir(os.path.join(filepath, dir)):
-            if not os.path.exists(os.path.join(rootDir, dir, item)):
-                os.makedirs(os.path.join(rootDir, dir, item))
-            with open(os.path.join(rootDir, dir, item, item+".txt"), 'w', encoding='utf-8') as ff:
-                counter=0
-                for fpath, dirs, fs in os.walk(os.path.join(filepath, dir, item)):
-                    for f in fs:
-                        try:
-                            with open(os.path.join(fpath,f),"r",encoding="utf-8") as f:
-                                for line in f:
-                                    if len(line)<=15 or len(line)>120:
-                                        continue
-                                    line = re.sub("\s", "", line)  # 去掉空格(也去掉了换行符)
-                                    line="  ".join(jieba.cut(line))
-                                    ff.write(line)
-                                    ff.write("\n")
-                                    counter+=1
-                                    if counter %10000==0:
-                                        print(dir, item, counter)
-                        except:
-                            print(fpath, f)
+    with open("./corpus/clean-八零年代纯女户奋斗史.txt", 'w', encoding='utf-8') as f:
+        counter = 0
+        with open("./corpus/八零年代纯女户奋斗史.txt", "r", encoding="utf-8") as ff:
+            for line in ff:
+                # if len(line) <= 15 or len(line) > 120:
+                #     continue
+                line = re.sub("\s", "", line)  # 去掉空格(也去掉了换行符)
+                line="  ".join(jieba.cut(line))
+                f.write(line)
+                f.write("\n")
+                counter += 1
+                if counter % 10000 == 0:
+                    print(counter)
                                 
-#========================================================================词向量批量训练=======================================================
+# ========================================================================词向量批量训练=======================================================
 if train_word2vector:
-    filepath="./corpus"
-    for dir in os.listdir(filepath):
-        if dir=="total":
-            continue
-        if not os.path.exists(os.path.join("word2vector", dir)):
-            os.makedirs(os.path.join("word2vector", dir))
-        
-        for item in os.listdir(os.path.join(filepath, dir)):
-            
-            if not os.path.exists(os.path.join("word2vector", dir, item)):
-                os.makedirs(os.path.join("word2vector", dir, item))
-            else:
-                continue
-            print(item, "开始训练词向量......")
-            model = Word2Vec(LineSentence(os.path.join(filepath, dir, item, item+".txt")), 
-                             size=256, 
-                             window=5, 
-                             min_count=1,
-                             workers=50, 
-                             iter=10)
-            model.save(os.path.join("word2vector", dir, item, "word2vector.model"))
-            print(item,"词向量训练完成，模型保存到：","/word2vector/",dir,"/",item,"/","word2vector.model")
+        path = "./corpus/clean-八零年代纯女户奋斗史.txt"
+        print("Begin train wordvector from {}".format(path))
+        model = Word2Vec(LineSentence(path),
+                         size=256,
+                         window=5,
+                         min_count=1,
+                         workers=50,
+                         iter=10)
+        model.save(os.path.join("./word2vector", "word2vector.model"))
+        print("wordvector training completed, and model have been saved to: {}"
+              .format(os.path.join("./word2vector", "word2vector.model")))
+
+if __name__ == "__main__":
+
+    from gensim.models import Word2Vec
+    model_path = "./word2vector/word2vector.model"
+    print("="*60)
+    print("Load word2vector model from: {}".format(model_path))
+    model = Word2Vec.load(model_path)
+    print(model)
+    print("=" * 60)
+    # ----------------------------相关词---------------------------------
+    query = "车祸"
+    try:
+        print(model.most_similar(query))
+    except:
+        print("该词不存在")
+
+
--- generation_by_word2vector_replace/edit_02/main.py
+++ generation_by_word2vector_replace/edit_02/main.py
@@ -1,8 +1,10 @@
 import os
 import json
 import pickle
+import jieba
 import itertools
 import argparse
+from random import sample
 import numpy as np
 from gensim.models import Word2Vec
 
@@ -10,137 +12,54 @@ from gensim.models import Word2Vec
 def parse_args():
     # 创建参数解析对象
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_keywords', type=int, default=2, help='the number of keywords')
-    parser.add_argument('--most_similarity', type=int, default=5, help='most_similarity')
-    parser.add_argument('--file_path', type=str, default="./corpus/total/total_segment.txt", help='filepath of corpus')
+    parser.add_argument('--most_similarity', type=int,
+                        default=2,
+                        help='most_similarity')
+    parser.add_argument('--sentence', type=str,
+                        default="",
+                        help='replace sentence')
+    parser.add_argument('--word2vector_model_path', type=str,
+                        default="./word2vector/word2vector.model",
+                        help='word2vector_model_path')
     return parser.parse_args()
 
 
 def main(args):
-    # 选择生成文本类型
-    print("请选择生成文本的类型：")
-    print("女频：")
-    print("0.农家种田    1.古言穿越    2.女尊天下    3.幻想言情    4.总裁霸爱    5.情深虐恋    6.浪漫青春    7.现代言情    8.存爱同人    9.蜜恋宠文    10.重生虐渣")
-    print("男频：")
-    print(
-        "11.修真异能  12.修真狂人   13.兵王传奇   14.异世奇遇   15.悬疑灵异   16.摸骨神医   17.架空历史   18.武侠仙侠   19.游戏竞技   20.热血爽文   21.玄幻魔幻   22.科幻末日   23.都市小说")
 
-    # ---------------------------------------------------------------------
-    with open("./category.json", "r", encoding="utf-8") as f:
-        category = json.load(f)
-    category_choose = input("请输入类别编号：")
-
-    # word2vector model list
-    word2vector_dir = []
-    for root, dirs, files in os.walk("./word2vector"):
-        for name in files:
-            if ".npy" not in os.path.join(root, name):
-                word2vector_dir.append(os.path.join(root, name))
-
-    # corpus list
-    corpus_dir = []
-    for root, dirs, files in os.walk("./corpus"):
-        for name in files:
-            corpus_dir.append(os.path.join(root, name))
-
-    for item in word2vector_dir:
-        if category[category_choose] in item:
-            word2vector_path = item
-
-    for item in corpus_dir:
-        if category[category_choose] in item:
-            corpus_path = item
-
+    # define dot set
+    dot = {"，", "。", "？", "！", "：", "：“", "。”", "？”", "！”"}
     # 加载word2vector模型
-    print("模型加载中......")
-    model = Word2Vec.load(word2vector_path)
+    model = Word2Vec.load(args.word2vector_model_path)
+    print("Loaded word2vector model from：{}".format(args.word2vector_model_path))
 
-    # ----------------------------------------------------------------------
-    # 输入关键词
-    string = []
-    for i in range(args.num_keywords):
-        temp = input("输入第" + str(i + 1) + "个关键词：")
-        while temp not in model.wv.vocab:
-            print("请换个关键词试试.....")
-            temp = input("输入第" + str(i + 1) + "个关键词：")
-        string.append(temp)
+    # ---------------- get similar word by word2vec for each word  ---------------
+    print("Please input sentence you wan to transform!"), exit()if len(args.sentence) == 0 else print("object is generating......")
 
-    # 通过word2vector寻找相关检索词
-    def find_similaritr_words(string):
-        index_words = []
-        for i in range(len(string)):
+    string = [i for i in jieba.cut(args.sentence)]
+    similarity_words_list = []
+    for word in string:
+        if word not in dot:
             try:
-                similarity_words = model.most_similar([string[i]])[:args.most_similarity]
-                temp = [item[0] for item in similarity_words]
-                temp.append(string[i])
-                index_words.append(temp)
+                similarity_words = model.most_similar([word])[:args.most_similarity]
+                similarity_words_list.append(similarity_words)
             except:
-                print("抱歉，没有关键词：", string[i])
-        return index_words
-
-    index_words = find_similaritr_words(string)
-
-    # print('相关检索词： ',index_words)
-
-    # 相关句子检索
-    def find_similarity_sentence(corpus_path, index_words):
-        for ii in range(len(index_words)):
-            for temp in itertools.combinations(index_words, len(index_words) - ii):
-                print("------------------------------------------------------------------------------------")
-                print('相关检索词： ', temp)
-                print("------------------------------------------------------------------------------------")
-                # 相关句子检索
-                matched_sentence = []
-                with open(corpus_path, "r", encoding="utf-8") as f:
-                    for line in f:
-                        line = list(line.strip().split(" "))
-                        if len(line) <= 100:  # 去掉长度大于100的句子
-                            match_num = 0
-                            for item in temp:
-                                if len(set(item) & set(line)) > 0:
-                                    match_num += 1
-                            if match_num == len(index_words) - ii:
-                                for i in index_words:
-                                    for j in i:
-                                        if j in line:
-                                            line[line.index(j)] = i[-1]
-                                print("".join(line))
-                                if line not in matched_sentence:
-                                    matched_sentence.append(line)
-                if len(matched_sentence) != 0:
-                    return matched_sentence
-
-    matched_sentence = find_similarity_sentence(corpus_path, index_words)
-
-    # 按照相似度排序
-    print("-------------------------------------------------------------------------------------------")
-    print("相关度排序中......")
-    print("-------------------------------------------------------------------------------------------")
-
-    # top_n 排序
-    def top_n(matched_sentence, n=5):
-        matched_similarity = []
-        for i in range(len(matched_sentence)):
-            temp = []
-            for w in matched_sentence[i]:
-                if w in model:
-                    temp.append(w)
-
-            matched_similarity.append(model.n_similarity(string, temp))
-        ordered_matched = [matched_sentence[i] for i in np.argsort(-np.array(matched_similarity))]
-        if len(ordered_matched) > n:
-            ordered_matched_top_n = ordered_matched[0:n]
+                similarity_words_list.append([word])
         else:
-            ordered_matched_top_n = ordered_matched
-        return ordered_matched_top_n
+            similarity_words_list.append([word])
 
-    ordered_matched_top_n = top_n(matched_sentence, 5)
-    for item in ordered_matched_top_n:
-        print("".join(item))
-    print("===============================================================================================")
+    # ----------------  create new sentence by similar words  ----------------------
+    new_sentence_list = []
+    for i in range(len(similarity_words_list)):
+        new_sentence_list.append(sample(similarity_words_list[i], 1)[0][0])
+    # trans list to string
+    new_sentence = "".join(new_sentence_list)
+    print("="*200)
+    print("|".join(string))
+    print("-"*200)
+    print("|".join(new_sentence_list))
+    print("=" * 200)
+    print(new_sentence)
 
-
-# ===========================================================================================================
 
 if __name__ == '__main__':
     main(parse_args())
